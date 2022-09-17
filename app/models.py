a1c550a24028608b3ab00558f75b96eb53683e11
YotoCoder <yotoelectronics@gmail.com>
2022-09-17 05:02:04 -0500

    agregado postgres

--- app/models.py
+++ app/models.py
@@ -6,8 +6,12 @@ class Note(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     alert = models.DateTimeField()
+    
+    price = models.FloatField()
+
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f'Nota: {self.name} | Fecha: {self.alert}'
--- core/settings.py
+++ core/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'notas',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': 1234
+    }   
 }
 
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
